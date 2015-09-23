331b2a32e357639a266df783a6f52e60649e583f
William <woverton@centiq.co.uk>
2015-09-23 14:56:32 +0100

    Fixed storage chart

--- baseCommands/stor.js
+++ baseCommands/stor.js
@@ -5,7 +5,7 @@ var StorageChartCommandHandler = function(){
 
 StorageChartCommandHandler.prototype.run = function(command, cParts, conn, screen, callback){
 
-	conn.exec("conn", "SELECT CONCAT(HOST, CONCAT(' - ', PATH)), CAST(USED_SIZE / 1024 / 1024 / 1024 AS INTEGER) AS USED_SIZE_GB, CAST(TOTAL_SIZE / 1024 / 1024 / 1024 AS INTEGER) AS TOTAL_SIZE_GB FROM SYS.M_DISKS", function(err, data) {
+	conn.exec("conn", "SELECT CONCAT(HOST, CONCAT(' - ', PATH)), CAST(USED_SIZE / 1024 / 1024 / 1024 AS INTEGER) AS USED_SIZE_GB, CAST(TOTAL_SIZE / 1024 / 1024 / 1024 AS INTEGER) - CAST(USED_SIZE / 1024 / 1024 / 1024 AS INTEGER) AS TOTAL_SIZE_GB FROM SYS.M_DISKS", function(err, data) {
 	    callback([err == null ? 0 : 1, err == null ? data : err, err == null ? "bar-chart" : "json", "Total Disk Usage"]);
 	})
 }
