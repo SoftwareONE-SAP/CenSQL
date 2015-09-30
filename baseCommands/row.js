e32f145957e102f2e586a62422deef48faf8f48b
William <woverton@centiq.co.uk>
2015-09-30 15:23:28 +0100

    For some reason, the row query needs an extra hours worth of data to complete

--- baseCommands/row.js
+++ baseCommands/row.js
@@ -7,7 +7,7 @@ RowStorageCommandHandler.prototype.run = function(command, cParts, conn, screen,
 
 	conn.exec("conn", "SELECT YEAR(SNAPSHOT_ID), MONTH(SNAPSHOT_ID), DAYOFMONTH(SNAPSHOT_ID), HOUR(SNAPSHOT_ID), HOST, SUM(USED_FIXED_PART_SIZE), MIN(SNAPSHOT_ID)\
         FROM _SYS_STATISTICS.GLOBAL_ROWSTORE_TABLES_SIZE\
-        WHERE SNAPSHOT_ID > ADD_DAYS(CURRENT_UTCTIMESTAMP, -3)\
+        WHERE SNAPSHOT_ID > ADD_SECONDS(ADD_DAYS(CURRENT_UTCTIMESTAMP, -3), -3600)\
         GROUP BY YEAR(SNAPSHOT_ID), MONTH(SNAPSHOT_ID), DAYOFMONTH(SNAPSHOT_ID), HOUR(SNAPSHOT_ID), HOST\
         ORDER BY YEAR(SNAPSHOT_ID) DESC, MONTH(SNAPSHOT_ID) DESC, DAYOFMONTH(SNAPSHOT_ID) DESC, HOUR(SNAPSHOT_ID) DESC, HOST DESC", function(err, data) {
         callback([err == null ? 0 : 1, err == null ? data : err, err == null ? "line-graph" : "json", "RS used fixed part size over the last 3 days", 3 * 24]);
