8a48e13814ebff49c07174706537668753399869
William <woverton@centiq.co.uk>
2015-11-03 15:41:12 +0000

    Table now uses cli-table module which keeps the columns the same size

--- dataFormatters/table.js
+++ dataFormatters/table.js
@@ -1,34 +1,72 @@
+var Table = require('cli-table');
+
 module.exports = function(command, data) {
 
     var lines = [];
 
-    var keys = [];
-
-    if (data && data.length > 0) {
-
-        keys = Object.keys(data[0]);
-
-        lines.push(keys.join(" | "))
-        lines.push(new Array(20).join("- "));
-    } else {
+    if (!data || data.length == 0) {
         lines.push("No Results\n");
         return lines;
     }
 
-    keys.reverse()
+    var keys = Object.keys(data[0]);
+
+    var table = new Table({
+        head: keys
+    });
+
+    // table.push(data);
 
     for (var k = 0; k < data.length; k++) {
         var rows = [];
 
-        for (var j = keys.length - 1; j >= 0; j--) {
-            rows.push(data[k][keys[j]])
+        for (var j = 0; j < keys.length; j++) {
+            
+            var value = data[k][keys[j]];
+
+            if(value == null) value = "NULL";
+
+            rows.push(value)
         };
 
-        var rowString = rows.join(" | ");
-
-        lines.push(rowString);
-
+        table.push(rows);
     };
 
-    return lines;
+    return table.toString().split("\n");
 }
+
+// Old non utf8 version
+// module.exports = function(command, data) {
+
+//     var lines = [];
+
+//     var keys = [];
+
+//     if (data && data.length > 0) {
+
+//         keys = Object.keys(data[0]);
+
+//         lines.push(keys.join(" | "))
+//         lines.push(new Array(20).join("- "));
+//     } else {
+//         lines.push("No Results\n");
+//         return lines;
+//     }
+
+//     keys.reverse()
+
+//     for (var k = 0; k < data.length; k++) {
+//         var rows = [];
+
+//         for (var j = keys.length - 1; j >= 0; j--) {
+//             rows.push(data[k][keys[j]])
+//         };
+
+//         var rowString = rows.join(" | ");
+
+//         lines.push(rowString);
+
+//     };
+
+//     return lines;
+// }
