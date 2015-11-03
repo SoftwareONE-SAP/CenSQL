ff107319480d26eaf060cf54c7f162dfb7d8e691
William <woverton@centiq.co.uk>
2015-11-03 13:24:16 +0000

    Added finished date to backup

--- baseCommands/ba.js
+++ baseCommands/ba.js
@@ -6,7 +6,7 @@ var SchemaViewCommandHandler = function(){
 SchemaViewCommandHandler.prototype.run = function(command, cParts, conn, screen, callback){
 
 	conn.exec("conn", 
-		"SELECT BACKUP_ID, UTC_START_TIME, STATE_NAME FROM sys.m_backup_catalog\
+		"SELECT BACKUP_ID, UTC_START_TIME, UTC_END_TIME, STATE_NAME, MESSAGE FROM sys.m_backup_catalog\
          WHERE ENTRY_TYPE_NAME = 'complete data backup'\
          ORDER BY entry_id DESC\
          LIMIT " + parseInt(cParts[1] && !isNaN(cParts[1]) ? cParts[1] : 10),
