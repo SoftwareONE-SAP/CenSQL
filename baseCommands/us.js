0b8ee23dd431f50b2f6c18f2f7e16cbcdbf62ff7
William <woverton@centiq.co.uk>
2015-11-03 17:24:47 +0000

    Added password_change_needed to users view

--- baseCommands/us.js
+++ baseCommands/us.js
@@ -5,7 +5,7 @@ var UserViewCommandHandler = function(){
 
 UserViewCommandHandler.prototype.run = function(command, cParts, conn, screen, callback){
 
-	conn.exec("conn", "SELECT USER_ID, USER_NAME, USER_MODE, LAST_SUCCESSFUL_CONNECT, USER_DEACTIVATED FROM SYS.USERS", function(err, data) {
+	conn.exec("conn", "SELECT USER_ID, USER_NAME, USER_DEACTIVATED, LAST_SUCCESSFUL_CONNECT, PASSWORD_CHANGE_NEEDED FROM SYS.USERS", function(err, data) {
 	    callback([err == null ? 0 : 1, err == null ? data : err, err == null ? "default" : "json"]);
 	})
 }
