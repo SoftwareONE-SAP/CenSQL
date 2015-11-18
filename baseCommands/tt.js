0c839fb711513675f8885d46a9968c080d29486e
William Overton <woverton@centiq.co.uk>
2015-11-18 18:05:01 +0000

    top tables now is in GB

--- baseCommands/tt.js
+++ baseCommands/tt.js
@@ -5,7 +5,7 @@ var TopTablesCommandHandler = function(){
 
 TopTablesCommandHandler.prototype.run = function(command, cParts, conn, screen, callback){
 
-	conn.exec("conn", "SELECT SCHEMA_NAME, TABLE_NAME, LOADED, MEMORY_SIZE_IN_TOTAL, ESTIMATED_MAX_MEMORY_SIZE_IN_TOTAL FROM SYS.M_CS_TABLES ORDER BY MEMORY_SIZE_IN_TOTAL DESC, MEMORY_SIZE_IN_DELTA DESC LIMIT " + parseInt(cParts[1] && !isNaN(cParts[1]) ? cParts[1] : 10), function(err, data) {
+	conn.exec("conn", "SELECT SCHEMA_NAME, TABLE_NAME, PART_ID, LOADED, MEMORY_SIZE_IN_TOTAL / 1024 / 1024 / 1024 AS TOTAL_MEMORY_GB, ESTIMATED_MAX_MEMORY_SIZE_IN_TOTAL / 1024 / 1024 / 1024 AS ESTIMATED_MAX_MEMORY_GB FROM SYS.M_CS_TABLES ORDER BY TOTAL_MEMORY_GB DESC, ESTIMATED_MAX_MEMORY_GB DESC LIMIT " + parseInt(cParts[1] && !isNaN(cParts[1]) ? cParts[1] : 10), function(err, data) {
         callback([err == null ? 0 : 1, err == null ? data : err, err == null ? "default" : "json"]);
     })
 }
