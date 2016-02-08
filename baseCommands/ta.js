ea06f7b37ea532d04b51502e8ddf1b4a9de514d9
William Overton <woverton@centiq.co.uk>
2016-02-08 09:50:04 +0000

    Updated ta command to show less information as it was showing too much. updated package.json for new version

--- baseCommands/ta.js
+++ baseCommands/ta.js
@@ -5,7 +5,7 @@ var TableViewCommandHandler = function(){
 
 TableViewCommandHandler.prototype.run = function(command, cParts, conn, screen, callback){
 
-	conn.exec("conn", "SELECT * FROM SYS.TABLES WHERE SCHEMA_NAME LIKE '" + cParts[1] + "'", function(err, data) {
+	conn.exec("conn", "SELECT TABLE_OID, SCHEMA_NAME, TABLE_NAME, TABLE_TYPE FROM SYS.TABLES WHERE SCHEMA_NAME LIKE '" + cParts[1] + "' ORDER BY TABLE_NAME", function(err, data) {
 	    callback([err == null ? 0 : 1, err == null ? data : err, err == null ? "default" : "json"]);
 	})
 }
