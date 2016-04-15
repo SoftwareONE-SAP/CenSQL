af3bf263b801d6e40720ca08300f7669c17a2f6e
William Overton <woverton@centiq.co.uk>
2016-04-15 16:21:28 +0100

    Now uses preview_size for tp if no count is supplied

--- util/baseCommands/tp.js
+++ util/baseCommands/tp.js
@@ -1,3 +1,4 @@
+var argv = require('optimist').argv;
 
 var TablePreviewCommandHandler = function(){
 	this.description = "";
@@ -5,7 +6,7 @@ var TablePreviewCommandHandler = function(){
 
 TablePreviewCommandHandler.prototype.run = function(command, cParts, conn, screen, callback){
 
-	var limit = parseInt(cParts[2] && !isNaN(cParts[2]) ? cParts[2] : 10)
+	var limit = parseInt(cParts[2] && !isNaN(cParts[2]) ? cParts[2] : (argv.preview_size || 10))
 
 	conn.exec("conn", "SELECT * FROM " + cParts[1] + " LIMIT " + limit, function(err, data) {
 	    callback([err == null ? 0 : 1, err == null ? data : err, err == null ? "default" : "json"]);
