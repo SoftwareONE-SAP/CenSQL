293226f02c5b9aff36c4faa721b9c4b810ce6618
William Overton <woverton@centiq.co.uk>
2016-04-27 17:31:45 +0100

    vs command now uses current_schema if no schema is provided

--- util/baseCommands/vs.js
+++ util/baseCommands/vs.js
@@ -5,6 +5,12 @@ var ViewsViewCommandHandler = function(){
 
 ViewsViewCommandHandler.prototype.run = function(command, cParts, conn, screen, callback){
 
+	if(!cParts[1]){
+		cParts[1] = "CURRENT_SCHEMA";
+	}else{
+		cParts[1] = "'" + cParts[1] + "'";
+	}
+
 	conn.exec("conn", "SELECT VIEW_NAME, VIEW_OID, IS_READ_ONLY, COMMENTS FROM SYS.VIEWS WHERE SCHEMA_NAME LIKE '" + cParts[1] + "'", function(err, data) {
 	    callback([err == null ? 0 : 1, err == null ? data : err, err == null ? "default" : "json"]);
 	})
