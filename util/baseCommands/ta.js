9920f39476f863be146e544214000bed9cf02269
William Overton <woverton@centiq.co.uk>
2016-04-27 17:30:25 +0100

    ta command now uses current_schema if no schema is provided

--- util/baseCommands/ta.js
+++ util/baseCommands/ta.js
@@ -5,7 +5,13 @@ var TableViewCommandHandler = function(){
 
 TableViewCommandHandler.prototype.run = function(command, cParts, conn, screen, callback){
 
-	conn.exec("conn", "SELECT TABLE_OID, TABLE_NAME, TABLE_TYPE FROM SYS.TABLES WHERE SCHEMA_NAME LIKE '" + cParts[1] + "' ORDER BY TABLE_NAME", function(err, data) {
+	if(!cParts[1]){
+		cParts[1] = "CURRENT_SCHEMA";
+	}else{
+		cParts[1] = "'" + cParts[1] + "'";
+	}
+
+	conn.exec("conn", "SELECT TABLE_OID, TABLE_NAME, TABLE_TYPE FROM SYS.TABLES WHERE SCHEMA_NAME LIKE " + cParts[1] + " ORDER BY TABLE_NAME", function(err, data) {
 	    callback([err == null ? 0 : 1, err == null ? data : err, err == null ? "default" : "json"]);
 	})
 }
