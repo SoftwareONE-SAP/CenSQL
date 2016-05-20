80b519baceda2441ffcc561907b37aecea59e619
William Overton <woverton@centiq.co.uk>
2016-05-20 10:31:08 +0100

    Renames util as it no longer majes sense

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-var ScreenManager = require("./util/ScreenManager.js");
-var CommandHandler = require("./util/CommandHandler.js");
+var ScreenManager = require("./bin/ScreenManager.js");
+var CommandHandler = require("./bin/CommandHandler.js");
 var HDB = require("./lib/HdbHandler.js");
 var argv = require('optimist').argv;
 var mkdirp = require('mkdirp');
@@ -10,26 +10,26 @@ var async = require("async");
 var osHomedir = require('os-homedir');
 var fs = require("fs");
 var pad = require("pad");
-var SavedConnectionManager = require("./util/SavedConnectionManager.js");
+var SavedConnectionManager = require("./bin/SavedConnectionManager.js");
 var CliTable = require('cli-table');
 
 var CenSql = function() {
 
     /**
-    * Bodge to fix hdb module
-    * @type {Boolean}
-    */
+     * Bodge to fix hdb module
+     * @type {Boolean}
+     */
     process.browser = true;
 
     /**
-    * get a global object for storing flags in
-    */
+     * get a global object for storing flags in
+     */
     GLOBAL.censql = {};
 
     /**
-    * Dont accept user input until we're ready
-    * @type {Boolean}
-    */
+     * Dont accept user input until we're ready
+     * @type {Boolean}
+     */
     GLOBAL.censql.RUNNING_PROCESS = true;
 
     /**
@@ -66,10 +66,14 @@ var CenSql = function() {
 
             this.connectToHdb(connDetails.host, connDetails.user, connDetails.pass, connDetails.port);
         } else {
-            this.screen.print("Connection '".red + argv.use.red.bold + ("' does not exist. Use the \\save command whilst connected to save a connection.").red, function() {
-                console.log();
-                process.exit(1)
-            })
+            if (argv.use.length > 0) {
+                this.screen.print("Connection '".red + argv.use.red.bold + ("' does not exist. Use the \\save command whilst connected to save a connection.").red, function() {
+                    console.log();
+                    process.exit(1)
+                })
+            } else {
+                this.showHelp();
+            }
         }
 
     }.bind(this))
@@ -99,15 +103,15 @@ CenSql.prototype.setTitle = function(isStudio) {
 
 CenSql.prototype.getSettings = function() {
     /**
-    * Load settings
-    */
+     * Load settings
+     */
     var settings = {};
 
     var settingsFilePath = path.join(osHomedir(), ".censql", "censql_settings");
 
     /**
-    * Save settings to file
-    */
+     * Save settings to file
+     */
     settings.save = function() {
         var self = JSON.parse(JSON.stringify(this));
 
@@ -119,8 +123,8 @@ CenSql.prototype.getSettings = function() {
     }
 
     /**
-    * load settings from file
-    */
+     * load settings from file
+     */
     settings.load = function() {
 
         try {
@@ -134,8 +138,8 @@ CenSql.prototype.getSettings = function() {
 
         } catch (e) {
             /**
-            * Since we had an error, this is likely to do with the file not existing. Lets save a default file and then load that
-            */
+             * Since we had an error, this is likely to do with the file not existing. Lets save a default file and then load that
+             */
             this.save();
             this.load();
         }
@@ -145,23 +149,23 @@ CenSql.prototype.getSettings = function() {
     settings.load();
 
     /**
-    * Set defaults
-    */
+     * Set defaults
+     */
     if (!settings.plotHeight) settings.plotHeight = 11;
     if (!settings.barHeight) settings.barHeight = 1;
     if (!("relativeGraphs" in settings)) settings.relativeGraphs = false;
-    
-    if(!("csv" in settings)){
+
+    if (!("csv" in settings)) {
         settings.csv = {};
     }
 
-    if(!("delimeter" in settings.csv)){
+    if (!("delimeter" in settings.csv)) {
         settings.csv.delimeter = ",";
     }
 
     /**
-    * Save the defaults
-    */
+     * Save the defaults
+     */
     settings.save();
 
     return settings;
@@ -172,34 +176,34 @@ CenSql.prototype.connectToHdb = function(host, user, pass, port) {
     this.hdb = new HDB();
 
     /**
-    * Connect to HANA with the login info supplied by the user
-    */
+     * Connect to HANA with the login info supplied by the user
+     */
     this.hdb.connect(host, user, pass, port, "conn", function(err, data) {
 
         if (err) {
-            this.screen.error(err.message + "\n", function(){
+            this.screen.error(err.message + "\n", function() {
                 process.exit(1);
             });
-            
+
             return;
         }
 
         /**
-        * If the user specified the command, we do not want to open an interactive session
-        */
+         * If the user specified the command, we do not want to open an interactive session
+         */
         if (!argv.command) {
             this.screen.ready.call(this.screen, this.hdb);
         }
 
         /**
-        * Create a new command handler
-        */
+         * Create a new command handler
+         */
         this.commandHandler = new CommandHandler(this.screen, this.hdb, argv.command, this.settings);
 
         /**
-        * Allow user inpput from now on
-        * @type {Boolean}
-        */
+         * Allow user inpput from now on
+         * @type {Boolean}
+         */
         GLOBAL.censql.RUNNING_PROCESS = false;
 
     }.bind(this))
@@ -208,15 +212,15 @@ CenSql.prototype.connectToHdb = function(host, user, pass, port) {
 CenSql.prototype.createScreen = function(settings, callback) {
 
     /**
-    * keep the colour var in settings just to make it easy to use, but we wont actually save it
-    * @type {[type]}
-    */
+     * keep the colour var in settings just to make it easy to use, but we wont actually save it
+     * @type {[type]}
+     */
     settings.colour = argv['no-colour'] || argv['no-color'] ? false : true;
     settings.studio = !!argv['studio'] || !!argv['s'];
 
     /**
-    * Create screen object adn give it the command handler to handle user input
-    */
+     * Create screen object adn give it the command handler to handle user input
+     */
     this.screen = new ScreenManager(
         // Command if given
         !!argv.command,
@@ -230,7 +234,7 @@ CenSql.prototype.createScreen = function(settings, callback) {
         }
     )
 
-    if(!argv.command){
+    if (!argv.command) {
         this.setTitle(settings.studio);
     }
 
@@ -239,38 +243,42 @@ CenSql.prototype.createScreen = function(settings, callback) {
 
 CenSql.prototype.showHelpTextIfNeeded = function(callback) {
     /**
-    * Make sure we have the arguments needed to connect to HANA
-    */
-    if (((!argv.host || !argv.user || !argv.pass || !argv.port) && !argv.use && !(argv.l || argv.list_connections)) || argv.help) {
-        console.log([
-            "Usage:\t censql --user <USER> --port 3<ID>15 --host <IP OR HOSTNAME> --pass <PASSWORD>",
-            "\t censql --user <USER> --port 3<ID>15 --host <IP OR HOSTNAME> --pass <PASSWORD> --command '<SQL_STRING>'",
-            "\t censql --use <ALIAS>",
-            "Example: censql --user SYSTEM --port 30015 --host 192.168.0.1 --pass Password123",
-            "Example: censql --user SYSTEM --port 30015 --host 192.168.0.1 --pass Password123 --command 'SELECT * FROM SYS.M_SERVICES'",
-            "Example: censql --use prd",
-            "",
-            "CenSQL Help",
-            "--user\t\tThe username for the user to connect as",
-            "--pass\t\tThe password for the user connecting with",
-            "--host\t\tThe host to connect to",
-            "--port\t\tThe port to connect to the host with (Layout: '3<ID>15', Instance 99 would be 39915)",
-            "--use <ALIAS>\tConnect using a saved connection instead of user,pass,host,port",
-            "",
-            "--command\t\tOptionally run a command/sql without entering the interective terminal",
-            "-s --studio\t\tEnter studio mode",
-            "",
-            "-l --list_connections\tOptionally run a command/sql without entering the interective terminal",
-            "--preview_size <COUNT>\tchange amount of rows shown in table preview in studio mode",
-            "",
-            "--no-colour\tdisable colour output",
-            "--no-color\talias of --no-colour",
-        ].join("\n"));
-
-        process.exit(0);
+     * Make sure we have the arguments needed to connect to HANA
+     */
+    if (((!argv.host || !argv.user || !argv.pass || !argv.port) && (!argv.use || argv.use.length == 0) && !(argv.l || argv.list_connections)) || argv.help) {
+        this.showHelp();
     }
 
     callback.call(this, null);
+}
+
+CenSql.prototype.showHelp = function() {
+    console.log([
+        "Usage:\t censql --user <USER> --port 3<ID>15 --host <IP OR HOSTNAME> --pass <PASSWORD>",
+        "\t censql --user <USER> --port 3<ID>15 --host <IP OR HOSTNAME> --pass <PASSWORD> --command '<SQL_STRING>'",
+        "\t censql --use <ALIAS>",
+        "Example: censql --user SYSTEM --port 30015 --host 192.168.0.1 --pass Password123",
+        "Example: censql --user SYSTEM --port 30015 --host 192.168.0.1 --pass Password123 --command 'SELECT * FROM SYS.M_SERVICES'",
+        "Example: censql --use prd",
+        "",
+        "CenSQL Help",
+        "--user\t\tThe username for the user to connect as",
+        "--pass\t\tThe password for the user connecting with",
+        "--host\t\tThe host to connect to",
+        "--port\t\tThe port to connect to the host with (Layout: '3<ID>15', Instance 99 would be 39915)",
+        "--use <ALIAS>\tConnect using a saved connection instead of user,pass,host,port",
+        "",
+        "--command\t\tOptionally run a command/sql without entering the interective terminal",
+        "-s --studio\t\tEnter studio mode",
+        "",
+        "-l --list_connections\tOptionally run a command/sql without entering the interective terminal",
+        "--preview_size <COUNT>\tchange amount of rows shown in table preview in studio mode",
+        "",
+        "--no-colour\tdisable colour output",
+        "--no-color\talias of --no-colour",
+    ].join("\n"));
+
+    process.exit(0);
 }
 
 CenSql.prototype.createFolderIfNeeded = function(callback) {
