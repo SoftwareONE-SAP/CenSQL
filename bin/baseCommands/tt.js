cddd80ddc47457cf2a5f39cecd63bf5617351e0e
William Overton <woverton@centiq.co.uk>
2016-10-11 11:56:45 +0100

    added schema selector to tt command

--- bin/baseCommands/tt.js
+++ bin/baseCommands/tt.js
@@ -5,7 +5,17 @@ var TopTablesCommandHandler = function(){
 
 TopTablesCommandHandler.prototype.run = function(command, cParts, conn, screen, callback){
 
-	conn.exec("conn", "SELECT SCHEMA_NAME, TABLE_NAME, RECORD_COUNT, (TABLE_SIZE / 1024 / 1024 / 1024) AS SIZE_GB, TABLE_TYPE, IS_PARTITIONED FROM M_TABLES ORDER BY TABLE_SIZE DESC LIMIT " + parseInt(cParts[1] && !isNaN(cParts[1]) ? cParts[1] : 10), function(err, data) {
+	var schemaLimit = "";
+	var rowLimit;
+
+	if(cParts[1] == "-s"){
+		schemaLimit = cParts[2];
+		rowLimit = parseInt(cParts[3] && !isNaN(cParts[3]) ? cParts[3] : 10);
+	}else{
+		rowLimit = parseInt(cParts[1] && !isNaN(cParts[1]) ? cParts[1] : 10);
+	}
+
+	conn.exec("conn", "SELECT SCHEMA_NAME, TABLE_NAME, RECORD_COUNT, (TABLE_SIZE / 1024 / 1024 / 1024) AS SIZE_GB, TABLE_TYPE, IS_PARTITIONED FROM M_TABLES " + (schemaLimit.length > 0 ? "WHERE SCHEMA_NAME = '" + schemaLimit + "'" : "") + " ORDER BY TABLE_SIZE DESC LIMIT " + rowLimit, function(err, data) {
         callback([err == null ? 0 : 1, err == null ? data : err, err == null ? "default" : "json"]);
     })
 }
