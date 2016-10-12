a74d2f243e1c0ca4a1e34f1b96e4ff7f7f015004
William Overton <woverton@centiq.co.uk>
2016-10-12 14:59:53 +0100

    lrs now show seconds correctly, and converted lrs to use optimist

--- bin/baseCommands/lrs.js
+++ bin/baseCommands/lrs.js
@@ -4,17 +4,15 @@ var LongRunningStatementsCommandHandler = function(){
 
 LongRunningStatementsCommandHandler.prototype.run = function(command, cParts, conn, screen, callback){
 
-	var timeLimit = 30;
-	var rowLimit;
+	var rowLimit = 10;
 
-	if(cParts[1] == "-t"){
-		timeLimit = parseInt(cParts[2] && !isNaN(cParts[2]) ? cParts[2] : 30);
-		rowLimit = parseInt(cParts[3] && !isNaN(cParts[3]) ? cParts[3] : 10);
-	}else{
-		rowLimit = parseInt(cParts[1] && !isNaN(cParts[1]) ? cParts[1] : 10);
+	argv = require('optimist')(cParts).default('t', 30).argv;
+
+	if (argv._.length > 1 && !isNaN(argv._[1])) {
+		rowLimit = parseInt(argv._[1]);
 	}
 
-	conn.exec("conn", "SELECT HOST, PORT, CONNECTION_ID, STATEMENT_ID, DB_USER, APP_USER, START_TIME, (DURATION_MICROSEC / 1000) AS DURATION_SECONDS, STATEMENT_STRING FROM SYS.M_EXECUTED_STATEMENTS WHERE DB_USER NOT IN ('_SYS_REPO', '_SYS_STATISTICS', 'SYS', '_SYS_TASK', '_SYS_AFL') AND DURATION_MICROSEC > (1000 * 1000 * " + timeLimit + ") ORDER BY START_TIME DESC LIMIT " + rowLimit, function(err, data) {
+	conn.exec("conn", "SELECT HOST, PORT, CONNECTION_ID, STATEMENT_ID, DB_USER, APP_USER, START_TIME, TO_INTEGER(DURATION_MICROSEC / 1000 / 1000) AS DURATION_SECONDS, STATEMENT_STRING FROM SYS.M_EXECUTED_STATEMENTS WHERE DB_USER NOT IN ('_SYS_REPO', '_SYS_STATISTICS', 'SYS', '_SYS_TASK', '_SYS_AFL') AND DURATION_MICROSEC > (1000 * 1000 * " + argv.t + ") ORDER BY START_TIME DESC LIMIT " + rowLimit, function(err, data) {
 	    callback([err == null ? 0 : 1, err == null ? data : err, err == null ? "default" : "json"]);
 	})
 }
