df9ca263ec58e826d028c9eb05bea046d6ef66a2
William Overton <woverton@centiq.co.uk>
2016-10-12 15:22:43 +0100

    Converted tt to use optimist

--- bin/baseCommands/tt.js
+++ bin/baseCommands/tt.js
@@ -5,17 +5,15 @@ var TopTablesCommandHandler = function(){
 
 TopTablesCommandHandler.prototype.run = function(command, cParts, conn, screen, callback){
 
-	var schemaLimit = "";
-	var rowLimit;
+	var rowLimit = 10;
 
-	if(cParts[1] == "-s"){
-		schemaLimit = cParts[2];
-		rowLimit = parseInt(cParts[3] && !isNaN(cParts[3]) ? cParts[3] : 10);
-	}else{
-		rowLimit = parseInt(cParts[1] && !isNaN(cParts[1]) ? cParts[1] : 10);
+	var argv = require('optimist')(cParts).default('s', "").argv;
+
+	if (argv._.length > 1 && !isNaN(argv._[1])) {
+		rowLimit = parseInt(argv._[1]);
 	}
 
-	conn.exec("conn", "SELECT SCHEMA_NAME, TABLE_NAME, RECORD_COUNT, (TABLE_SIZE / 1024 / 1024 / 1024) AS SIZE_GB, TABLE_TYPE, IS_PARTITIONED FROM M_TABLES " + (schemaLimit.length > 0 ? "WHERE SCHEMA_NAME = '" + schemaLimit + "'" : "") + " ORDER BY TABLE_SIZE DESC LIMIT " + rowLimit, function(err, data) {
+	conn.exec("conn", "SELECT SCHEMA_NAME, TABLE_NAME, RECORD_COUNT, (TABLE_SIZE / 1024 / 1024 / 1024) AS SIZE_GB, TABLE_TYPE, IS_PARTITIONED FROM M_TABLES " + (argv.s.length > 0 ? "WHERE SCHEMA_NAME = '" + argv.s + "'" : "") + " ORDER BY TABLE_SIZE DESC LIMIT " + rowLimit, function(err, data) {
         callback([err == null ? 0 : 1, err == null ? data : err, err == null ? "default" : "json"]);
     })
 }
