7e593f06bfa538b8cccbad5f7eb22ab4ba1a16f3
William Overton <woverton@centiq.co.uk>
2016-10-27 16:52:10 +0100

    Added minimum rating option to alerts command

--- bin/baseCommands/al.js
+++ bin/baseCommands/al.js
@@ -5,7 +5,13 @@ var AlertsCommandHandler = function(){
 
 AlertsCommandHandler.prototype.run = function(command, cParts, conn, screen, callback){
 
-	conn.exec("conn", "SELECT ALERT_TIMESTAMP, ALERT_RATING, ALERT_NAME, ALERT_DETAILS, INDEX FROM _SYS_STATISTICS.STATISTICS_CURRENT_ALERTS", function(err, data) {
+	var minVal = 0;
+
+	if(!isNaN(cParts[1])){
+		minVal = parseInt(cParts[1]);
+	}
+
+	conn.exec("conn", "SELECT ALERT_TIMESTAMP, ALERT_RATING, ALERT_NAME, ALERT_DETAILS, INDEX FROM _SYS_STATISTICS.STATISTICS_CURRENT_ALERTS WHERE ALERT_RATING >= " + minVal, function(err, data) {
 	    callback([err == null ? 0 : 1, err == null ? data : err, err == null ? "default" : "sql-error"]);
 	})
 }
