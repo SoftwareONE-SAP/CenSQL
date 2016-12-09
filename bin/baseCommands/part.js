f4c561e680cdbc471d3f53a203dbd79a2e633c6b
William Overton <woverton@centiq.co.uk>
2016-12-09 10:56:01 +0000

    Added loaded column to part command

--- bin/baseCommands/part.js
+++ bin/baseCommands/part.js
@@ -24,7 +24,10 @@ ParitionsCommandHandler.prototype.run = function(command, cParts, conn, screen, 
 		table = table.substring(1, table.length - 1);
 	}
 
-	conn.exec("conn", "SELECT T.PART_ID, P.RANGE, T.RECORD_COUNT FROM M_CS_TABLES AS T JOIN M_CS_PARTITIONS AS P ON (T.SCHEMA_NAME = p.SCHEMA_NAME AND T.TABLE_NAME = P.TABLE_NAME AND T.PART_ID = P.PART_ID) WHERE T.SCHEMA_NAME = " + schema + " AND T.TABLE_NAME = '" + table + "' ORDER BY P.PART_ID ASC", function(err, data) {
+	conn.exec("conn", "SELECT T.PART_ID, T.LOADED, P.RANGE, T.RECORD_COUNT \
+		FROM M_CS_TABLES AS T \
+		JOIN M_CS_PARTITIONS AS P ON (T.SCHEMA_NAME = p.SCHEMA_NAME AND T.TABLE_NAME = P.TABLE_NAME AND T.PART_ID = P.PART_ID) \
+		WHERE T.SCHEMA_NAME = " + schema + " AND T.TABLE_NAME = '" + table + "' ORDER BY P.PART_ID ASC", function(err, data) {
 	    callback([err == null ? 0 : 1, err == null ? data : err, err == null ? "default" : "sql-error"]);
 	})
 }
