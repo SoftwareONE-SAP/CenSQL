88214b6e517371544a4e2da1471974c5d8f6533c
William Overton <woverton@centiq.co.uk>
2017-03-30 11:30:25 +0100

    Added size to part command

--- bin/baseCommands/part.js
+++ bin/baseCommands/part.js
@@ -24,7 +24,7 @@ ParitionsCommandHandler.prototype.run = function(command, cParts, conn, screen, 
 		table = table.substring(1, table.length - 1);
 	}
 
-	conn.exec("conn", "SELECT T.PART_ID, T.LOADED, P.RANGE, T.RECORD_COUNT \
+	conn.exec("conn", "SELECT T.PART_ID, T.LOADED, P.RANGE, T.RECORD_COUNT, CAST(T.MEMORY_SIZE_IN_TOTAL / 1024 / 1024 AS DECIMAL(10, 2)) AS SIZE_GB, CAST(T.ESTIMATED_MAX_MEMORY_SIZE_IN_TOTAL / 1024 / 1024 AS DECIMAL(10, 2)) AS MAX_SIZE_GB \
 		FROM M_CS_TABLES AS T \
 		JOIN M_CS_PARTITIONS AS P ON (T.SCHEMA_NAME = p.SCHEMA_NAME AND T.TABLE_NAME = P.TABLE_NAME AND T.PART_ID = P.PART_ID) \
 		WHERE T.SCHEMA_NAME = " + (!schema ? "CURRENT_SCHEMA" : "?") + " AND T.TABLE_NAME = ? ORDER BY P.PART_ID ASC", (!schema ? [table] : [schema, table]), function(err, data) {
