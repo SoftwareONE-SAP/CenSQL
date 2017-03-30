6691697c6aea580b35eb907e92a53cceeb06bf01
William Overton <woverton@centiq.co.uk>
2017-03-30 11:30:08 +0100

    Added count of actions to con command

--- bin/baseCommands/con.js
+++ bin/baseCommands/con.js
@@ -5,7 +5,24 @@ var ConnectionsCommandHandler = function(){
 
 ConnectionsCommandHandler.prototype.run = function(command, cParts, conn, screen, callback){
 
-	conn.exec("conn", "SELECT HOST, USER_NAME, COUNT(*) AS AMOUNT FROM SYS.M_CONNECTIONS WHERE CONNECTION_STATUS = 'RUNNING' OR CONNECTION_STATUS = 'IDLE' AND AUTHENTICATION_METHOD <> 'INTERNAL' GROUP BY HOST, USER_NAME", function(err, data) {
+	var sql = "\
+		SELECT \
+			C.HOST, \
+			C.USER_NAME, \
+			COUNT(*) AS AMOUNT, \
+			SUM(S.SELECT_EXECUTION_COUNT) AS SELECTS,\
+			SUM(S.UPDATE_COUNT) AS UPDATES,\
+			SUM(S.ROLLBACK_COUNT) AS ROLLBACKS,\
+			MAX(LAST_EXECUTED_TIME) AS LAST_EXECUTED_TIME\
+		FROM SYS.M_CONNECTIONS AS C \
+		JOIN SYS.M_CONNECTION_STATISTICS AS S ON (C.CONNECTION_ID = S.CONNECTION_ID)\
+		WHERE \
+			(C.CONNECTION_STATUS = 'RUNNING' OR C.CONNECTION_STATUS = 'IDLE') \
+			AND C.AUTHENTICATION_METHOD <> 'INTERNAL' \
+		GROUP BY C.HOST, C.USER_NAME \
+		ORDER BY C.HOST, C.USER_NAME"
+
+	conn.exec("conn", sql, function(err, data) {
 	    callback([err == null ? 0 : 1, err == null ? data : err, err == null ? "default" : "sql-error"]);
 	})
 }
