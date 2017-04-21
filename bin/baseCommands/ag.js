3642f3383d848ab933a68a291e57ce782593ccd2
William Overton <woverton@centiq.co.uk>
2017-04-21 11:25:30 +0100

    removed unwanted columns from bin/baseCommands/ag.js

--- bin/baseCommands/ag.js
+++ bin/baseCommands/ag.js
@@ -5,7 +5,7 @@ var AgentsCommandHandler = function(){
 
 AgentsCommandHandler.prototype.run = function(command, cParts, conn, screen, callback){
 
-	conn.exec("conn", "SELECT * FROM SYS.M_AGENTS", function(err, data) {
+	conn.exec("conn", "SELECT AGENT_NAME AS NAME, AGENT_VERSION, AGENT_STATUS AS STATUS, LAST_CONNECT_TIME, USED_PHYSICAL_MEMORY, FREE_PHYSICAL_MEMORY, USED_SWAP_SPACE, FREE_SWAP_SPACE FROM SYS.M_AGENTS", function(err, data) {
 	    callback([err == null ? 0 : 1, err == null ? data : err, err == null ? "default" : "sql-error"]);
 	})
 }
