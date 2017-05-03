9c6998d7c954d8568f774ba054b53c9ad14fe6a3
William Overton <woverton@centiq.co.uk>
2017-05-03 10:52:43 +0100

    Removed memory usage from wl command as it seems to always be -1

--- bin/baseCommands/wl.js
+++ bin/baseCommands/wl.js
@@ -5,7 +5,7 @@ var WorkloadCommandHandler = function(){
 
 WorkloadCommandHandler.prototype.run = function(command, cParts, conn, screen, callback){
 
-	conn.exec("conn", "SELECT HOST, PORT, CURRENT_EXECUTION_RATE, CURRENT_COMPILATION_RATE, CURRENT_UPDATE_TRANSACTION_RATE, CURRENT_TRANSACTION_RATE, CURRENT_COMMIT_RATE, CURRENT_ROLLBACK_RATE, CURRENT_MEMORY_USAGE_RATE FROM M_WORKLOAD", function(err, data) {
+	conn.exec("conn", "SELECT HOST, PORT, CURRENT_EXECUTION_RATE, CURRENT_COMPILATION_RATE, CURRENT_UPDATE_TRANSACTION_RATE, CURRENT_TRANSACTION_RATE, CURRENT_COMMIT_RATE, CURRENT_ROLLBACK_RATE FROM M_WORKLOAD", function(err, data) {
 	    callback([err == null ? 0 : 1, err == null ? data : err, err == null ? "default" : "sql-error"]);
 	})
 }
